3265eafbd16cff38c3fb0f41fb5e9341be0229d3
unknown <omerchetin19@gmail.com>
2021-05-03 09:25:11 +0300

    adding responsive homepage content

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className='home'>
             <div className="homeLeft">
-                <div className="homeLeftTitle">robert brown</div>
+                <div className="homeLeftTitle">omar chatin</div>
                 <div className="homeLeftBody">profitional <br/> ux/ui desginer</div>
                 <button className="learnMoreBtn">learn More</button>
             </div>
