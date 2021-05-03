5da45ee3d1bd4515e209ed0df2f4852fccf3a21d
unknown <omerchetin19@gmail.com>
2021-05-03 07:25:41 +0300

    test commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {useState} from 'react'
+export default function Navbar() {
+
+    const [ShowMenuStyle, setShowMenuStyle] = useState(false)
+    const showMenuBar = (e)=>{
+        e.preventDefault()
+        setShowMenuStyle(!ShowMenuStyle)
+    }
+
+
+    return (
+        <nav>
+            <div className="leftNav">
+                <div className="leftNavText">omar chatin</div>
+            </div>
+            <div className="rightNav" id={ShowMenuStyle ? 'showMenuList' : 'hideMenuList'}>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">contacts</a></li>
+                </ul>
+            </div>
+            <div className="menuBarBtn" onClick={showMenuBar}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+        </nav>
+    )
+}
