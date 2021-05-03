d253a6d825d4b07309a0adde03e04911a5cfabb4
unknown <omerchetin19@gmail.com>
2021-05-03 15:21:33 +0300

    seperateing css into diffeterent files in (styles) folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/Navbar";
-import Home from "./components/Home";
+import Navbar from "./pages/Navbar";
+import Home from "./pages/Home";
 import './App.css'
 
 function App() {
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
+
 export default function Navbar() {
 
     const [ShowMenuStyle, setShowMenuStyle] = useState(false)
