6c4ca7f262f6bea18604b51628f11c2a1199c058
unknown <omerchetin19@gmail.com>
2021-06-17 14:30:20 +0300

    adding some theme changeing animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import React,{useState} from 'react'
-import {Body, NavBar} from './styles/style.jsx'
+import {Body, NavBar, Firstsection, Hr} from './styles/style.jsx'
 import './App.css'
 import moon from './imgs/moon.png'
+import sun from './imgs/sun.png'
+import me1 from './imgs/me1.png'
+
 function App() {
   const [Theme, setTheme] = useState(true)
 
   return (
     <Body Theme={Theme}>
-      <NavBar Theme={Theme}>
+      <NavBar className='NavBar' Theme={Theme}>
         <div className="left-nav">
-          <img src={moon} onClick={()=>setTheme(!Theme)} alt=""/>
+          <img className={Theme ? 'change-theme' : ''} src={moon} onClick={()=>setTheme(!Theme)} alt=""/>
+          <img className={!Theme ? 'change-theme' : ''} src={sun} onClick={()=>setTheme(!Theme)} alt=""/>
         </div>
         <div className="right-nav">
           <li><a href="">home</a></li>
@@ -18,6 +22,20 @@ function App() {
           <li><a href="">contactme</a></li>
         </div>
       </NavBar>
+
+
+      <Firstsection Theme={Theme} className="first-section">
+        <img src={me1} className='me1' alt=""/>
+        <div className="p1">👋 hi there , i’m omar chatin</div>
+        <div className="p2">i’m ux/ui desginer and front end web developer</div>
+        <Hr Theme={Theme}></Hr>
+      </Firstsection>
+
+      <div className="showMenuBar">
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
+
     </Body>
   )
 }
--- src/styles/style.jsx
+++ src/styles/style.jsx
@@ -11,5 +11,19 @@ export const NavBar = styled.div`
     display: flex;
     justify-content: space-between;
     width: calc(100%-5px);
-    padding: 10px;
+    padding: 10px 10px 20px 10px;
+`
+
+export const Firstsection = styled.div`
+color: ${(props)=>props.Theme ? 'white' : 'black'};
+`
+
+export const Hr = styled.div`
+width: 60%;
+height: 5px;
+background-color: ${(props)=>props.Theme ? '#9D9D9D' : 'red'};
+margin: auto;
+border-radius: 10px;
+margin-top: 65px;
+margin-bottom: 65px;
 `
