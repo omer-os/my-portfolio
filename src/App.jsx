487af13815cddf878988f2fc5d237ea7adc6eb37
unknown <omerchetin19@gmail.com>
2021-06-23 18:47:33 +0300

    close to finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
-import {Body,ShowMenuBar , Rightnav, NavBar, Firstsection, Hr} from './styles/style.jsx'
+import {Body, ContactWithMe, Proj, Projects, AboutMe,ShowMenuBar , Rightnav, NavBar, Firstsection, Hr} from './styles/style.jsx'
 
 import './App.css'
 import moon from './imgs/moon.png'
 import sun from './imgs/sun.png'
 import me1 from './imgs/me1.png'
-
+import component6 from './imgs/component6.png'
 function App() {
   const [Theme, setTheme] = useState(true)
   const [ShowNav, setShowNav] = useState(false)
@@ -36,6 +36,71 @@ function App() {
         <div className="line1"></div>
         <div className="line2"></div>
       </ShowMenuBar>
+
+      <AboutMe>
+        <h1 style={{'textAlign': 'center'}}>About Me</h1>
+        <br/><br/>
+        <div className="aboutMeTop aboutMeDiv">
+          I’m studying dental Industry in al kitab university in iraq/kirkuk .
+        </div>
+        <div className="aboutMeBottom aboutMeDiv">
+          I love building websites and share my knowledge with everyone ! 
+        </div>
+      </AboutMe>
+      <Hr Theme={Theme}></Hr>
+      <Projects>
+        <h1 style={{'textAlign': 'center','width':'100%','font-size':'6vmin'}}>My Projects</h1>
+
+        <Proj>
+          <div className="projTitle">Note Keeper App</div>
+          <div className="projBody">Created using reactjs and styled components</div>
+          <div className="githubLink">github Link</div>
+        </Proj>
+        <Proj>
+          <div className="projTitle">Note Keeper App</div>
+          <div className="projBody">Created using reactjs and styled components</div>
+          <div className="githubLink">github Link</div>
+        </Proj>
+        <Proj>
+          <div className="projTitle">Note Keeper App</div>
+          <div className="projBody">Created using reactjs and styled components</div>
+          <div className="githubLink">github Link</div>
+        </Proj>
+        <Proj>
+          <div className="projTitle">Note Keeper App</div>
+          <div className="projBody">Created using reactjs and styled components</div>
+          <div className="githubLink">github Link</div>
+        </Proj>
+      </Projects>
+
+      <Hr Theme={Theme}></Hr>
+      <ContactWithMe>
+        <h1 style={{'textAlign': 'center','width':'100%','font-size':'6vmin'}}>Contacts</h1>
+
+        <div className="contact1">
+          <h1 style={{'textAlign': 'center','width':'100%','font-size':'3.5vmin', 'margin':'40px 0','color':'gray'}} className="contact1-1">
+            Follow Me On
+          </h1>
+          <div className="contact1-2">
+            <div className="contact1-2-1item">
+              <div className="ContactItemTop">stackoverflow</div>
+              <div className="ContactItemBottom"><img src={component6} alt=""/></div>
+            </div>
+            <div className="contact1-2-1item">
+              <div className="ContactItemTop">telegram</div>
+              <div className="ContactItemBottom"><img src={component6} alt=""/></div>
+            </div>
+            <div className="contact1-2-1item">
+              <div className="ContactItemTop">instagram</div>
+              <div className="ContactItemBottom"><img src={component6} alt=""/></div>
+            </div>
+            <div className="contact1-2-1item">
+              <div className="ContactItemTop">github</div>
+              <div className="ContactItemBottom"><img src={component6} alt=""/></div>
+            </div>
+          </div>
+        </div>
+      </ContactWithMe>
     </Body>
   )
 }
--- src/styles/style.jsx
+++ src/styles/style.jsx
@@ -8,6 +8,10 @@ height: 100vh;
 `
 
 export const NavBar = styled.div`
+    position: fixed;
+    left: 0px;
+    right: 0px;
+    top: 0px;
     background-color: ${(props)=> props.Theme ? 'black' : '#1A2F40'};
     display: flex;
     justify-content: space-between;
@@ -32,9 +36,10 @@ margin-bottom: 65px;
 `
 
 export const ShowMenuBar = styled.div`
-    position: absolute;
+    position: fixed;
     top: 10px;
     right: 15px;
+    z-index: 200;
     width: fit-content;
     display: none;
     flex-direction: column;
@@ -47,8 +52,105 @@ export const ShowMenuBar = styled.div`
 export const Rightnav = styled.div`
     @media (max-width: 550px){
             top: ${props=>props.ShowNav ? '50px' : '-200px'};
-            background-color: #000000bc;
+           background-color: #000000bc;
             padding-bottom: 15px;
     }
 
+`
+
+
+export const AboutMe = styled.div`
+    margin: auto;
+    h1{
+        font-size: 6vmin;
+    }
+    .aboutMeDiv{
+        width: 20%;
+        padding: 2.5%;
+        border: 1px solid gray;
+        margin: 0 20%;
+        border-radius: 8px;
+        font-size: 3.6vmin;
+    }
+
+    .aboutMeBottom{
+        position: relative;
+        right: calc(-140px - 20%);
+    }
+
+    @media (max-width: 800px){
+        .aboutMeDiv{
+            width: 60%;
+            margin: auto;
+        }.aboutMeBottom{
+            float: none;
+            margin-top: 30px;
+            right: 0px;
+        }
+    }
+`
+
+export const Projects = styled.div`
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    margin: 10px 20%;
+    justify-content: center;
+    @media (max-width: 350px){
+        margin: 10px 0;
+    }
+`
+export const Proj = styled.div`
+    width: 250px;
+    height: fit-content;
+    text-align: center;
+    border: 2px solid white;
+    border-radius: 10px;
+    padding: 5% 3%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 5%;
+    .projTitle{
+        font-size: 4.3vmin;
+        margin:10px 0;
+    }
+    .projBody{
+        width: 90%;
+        color: #858585;
+        margin:10px 0;
+        font-size: 2.5vmin;
+    }
+    .githubLink{
+        color: blue;
+        margin:10px 0;
+        font-size: 2.5vmin;
+    }
+    @media (max-width: 350px){
+        .projTitle,.projBody,.githubLink{
+            font-size: 5vmin;
+        }
+    }
+`
+
+export const ContactWithMe = styled.div`
+    width: 70%;
+    margin: auto;
+    .contact1-2{
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .contact1-2-1item{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        text-transform: capitalize;
+    }.contact1-2-1item img{
+        width: 60%;
+        margin-top: 10px;
+    }
 `
