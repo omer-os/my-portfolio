125ef3c5a735769b4e5a37b9ff6e9817a0b9b09f
omar chatin <omerchatin19@gmail.com>
2021-11-22 15:43:30 +0300

    added email service

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,9 +1,57 @@
-import { Button, Card, CardContent, Grid, TextField, Typography } from '@mui/material';
-import React from 'react';
+import { Button, Card, CardContent, Grid, Slide, TextField, Typography } from '@mui/material';
+import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 
+
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 function Contact() {
+
+
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_ovzv73k', 'template_5aas0ya', form.current, 'user_060QhAv56WoEpD09EeJx5')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+
+
+  const SendBtnHandeler=()=>{
+    handleClickOpen()
+  }
+
+
+
+
+
   return (
     <div style={{margin:"20px 0"}}> 
       <Grid>
@@ -15,22 +63,22 @@ function Contact() {
             <Typography variant="body2" color="textSecondary" component="p" gutterBottom>
               Fill up the form and I'll get back to you within 24 hours.
           </Typography> 
-            <form>
+            <form ref={form} onSubmit={sendEmail}>
               <Grid container spacing={1}>
                 <Grid xs={12} sm={6} item>
-                  <TextField placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
+                  <TextField name="user_name" placeholder="Enter first name" label="First Name" variant="outlined" fullWidth required />
                 </Grid>
                 <Grid xs={12} sm={6} item>
-                  <TextField placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
+                  <TextField name="user_last_name" placeholder="Enter last name" label="Last Name" variant="outlined" fullWidth required />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField type="email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
+                  <TextField type="email" name="user_email" placeholder="Enter email" label="Email" variant="outlined" fullWidth required />
                 </Grid>
                 <Grid item xs={12}>
-                  <TextField label="Message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
+                  <TextField label="Message" name="message" multiline rows={4} placeholder="Type your message here" variant="outlined" fullWidth required />
                 </Grid>
                 <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="primary" fullWidth>Submit</Button>
+                  <Button type="submit" variant="contained" color="primary" onClick={()=>SendBtnHandeler()} fullWidth>Submit</Button>
                 </Grid>
 
               </Grid>
@@ -42,8 +90,23 @@ function Contact() {
 
 
 
-
-      
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"heyyyyy !"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            your email sent successfully, ill reach you soon with your email . thank you !
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Ok</Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
